4e3c24c8209ec5ea8baaefaea9f9fa0bc8442d0f
Mario-Chavez <marioachavez1@gmail.com>
2023-04-10 11:34:23 -0300

    termino la clase

--- js/administrador.js
+++ js/administrador.js
@@ -1,18 +1,19 @@
 import { Pelicula } from "./classPelicula.js";
 
-let editar = document.getElementById("editar");
-editar.addEventListener("click", function () {
-    console.log("hice click");
-});
-// crear nueva peliculas
-let nuevaPeli = new Pelicula(
-    "mario",
-    "algo",
-    "otraCosa",
-    "aventura",
-    2012,
-    "1 h",
-    "eeuu",
-    "mario"
-);
-console.log(nuevaPeli);
+let btnEditar = document.getElementById("editar");
+btnEditar.addEventListener("click", crearPeli);
+
+function crearPeli() {
+    // crear nueva peliculas
+    let nuevaPeli = new Pelicula(
+        "mario",
+        "algo",
+        "otraCosa",
+        "aventura",
+        2012,
+        "1 h",
+        "eeuu",
+        "mario"
+    );
+    console.log(nuevaPeli);
+}
